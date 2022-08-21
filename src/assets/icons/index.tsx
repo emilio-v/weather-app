c427f78b95a7e66d71e9ac74931273d8b5b44dbe
emilio-v <emilio_785@hotmail.com>
2022-08-21 03:29:44 -0500

    added icons

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,16 +1,112 @@
-// Location Icon
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  className="h-5 w-5"
-  viewBox="0 0 20 20"
-  fill="currentColor"
->
-  <path
-    fill-rule="evenodd"
-    d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-    clip-rule="evenodd"
-  />
-</svg>;
+import { SVGProps } from "react";
+
+export const LocationIcon = ({
+  width = "24px",
+  color = "black",
+  ...props
+}: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height="auto"
+    viewBox="0 0 20 20"
+    fill={color}
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const CloseIcon = ({
+  width = "24px",
+  color = "black",
+  ...props
+}: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height="auto"
+    viewBox="0 0 20 20"
+    fill={color}
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const RefreshIcon = ({
+  width = "24px",
+  color = "black",
+  ...props
+}: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke={color}
+    strokeWidth={2}
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+    />
+  </svg>
+);
+
+export const PlusIcon = ({
+  width = "24px",
+  color = "black",
+  ...props
+}: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height="auto"
+    viewBox="0 0 20 20"
+    fill={color}
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const MenuIcon = ({
+  width = "24px",
+  color = "black",
+  ...props
+}: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height="auto"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke={color}
+    strokeWidth={2}
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+);
 
 // Spinner Loader
 <svg
