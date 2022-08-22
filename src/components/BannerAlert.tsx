dc33655241a8810973e68d4e02da9e1fe7c513d6
emilio-v <emilio_785@hotmail.com>
2022-08-22 02:55:58 -0500

    created banner alert

---
+++ src/components/BannerAlert.tsx
@@ -0,0 +1,33 @@
+import { CloseIcon, NotificationIcon } from "../assets/icons";
+
+interface BannerAlertProps {
+  message?: string;
+}
+
+const defaultMessage = "An error has occurred.";
+
+const BannerAlert = ({ message = defaultMessage }: BannerAlertProps) => {
+  return (
+    <div className="bg-indigo-600 max-w-5xl rounded-md">
+      <div className="flex py-3 px-3 sm:px-6 lg:px-8 flex items-center justify-between flex-wrap">
+        <div className="w-0 flex-1 flex items-center">
+          <span className="flex p-2 rounded-lg bg-indigo-800">
+            <NotificationIcon color="#FFF" />
+          </span>
+          <p className="ml-3 font-medium text-white truncate">{message}</p>
+        </div>
+        <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+          <button
+            type="button"
+            className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+          >
+            <span className="sr-only">Dismiss</span>
+            <CloseIcon color="#FFF" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BannerAlert;
