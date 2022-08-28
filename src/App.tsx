ce805010a31546468b469fd5d1e61950ecc54b0b
emilio-v <emilio_785@hotmail.com>
2022-08-28 02:35:11 -0500

    relocate files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AppProvider } from "./context/WheaterContext/context";
+import { AppProvider } from "./context/context";
 import MainLayout from "./layouts/MainLayout";
 import Weather from "./pages/Weather";
 
