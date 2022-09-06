4bc458bf4aaf825acf3699c354fa5b0b6b25239e
emilio-v <emilio_785@hotmail.com>
2022-09-06 00:42:28 -0500

    created modal to display search input, add city from search input

--- src/components/AddLocationButton.tsx
+++ src/components/AddLocationButton.tsx
@@ -1,19 +1,36 @@
-import { useContext } from "react";
+import { useState } from "react";
 import { PlusIcon } from "../assets/icons";
-import { addLocation } from "../context/actions";
-import { AppContext } from "../context/context";
+import SearchInput from "./SearchInput";
 
 const AddLocationButton = () => {
-  const { dispatch } = useContext(AppContext);
+  const [showModal, setShowModal] = useState(false);
   return (
-    <div className="flex justify-center mb-32 mt-8">
-      <button
-        className="p-4 bg-oxfordBlue-300 rounded"
-        onClick={() => dispatch(addLocation(`New Location - ${Math.random()}`))}
-      >
-        <PlusIcon color="white" />
-      </button>
-    </div>
+    <>
+      <div className="flex justify-center mb-32 mt-8">
+        <button
+          className="p-4 bg-oxfordBlue-300 rounded"
+          onClick={() => setShowModal(true)}
+        >
+          <PlusIcon color="white" />
+        </button>
+      </div>
+      {showModal && (
+        <>
+          <div
+            className="p-10 justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            onClick={() => setShowModal(false)}
+          >
+            <div
+              className="relative w-full my-6 mx-auto max-w-md"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <SearchInput onClose={() => setShowModal(false)} />
+            </div>
+          </div>
+          <div className="fixed z-30 min-w-full min-h-screen inset-0 opacity-30 bg-[#2d2e4f8a]" />
+        </>
+      )}
+    </>
   );
 };
 
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,23 +1,31 @@
-import { FormEvent } from "react";
+import { useContext, FormEvent } from "react";
+import useForms from "../hooks/useForms";
+import { addLocation } from "../context/actions";
+import { AppContext } from "../context/context";
 import { SendIcon } from "../assets/icons";
 
-import useForms from "../hooks/useForms";
+type SearchInputProps = {
+  onClose: () => void;
+};
 
-const SearchInput = () => {
+const SearchInput = ({ onClose }: SearchInputProps) => {
+  const { dispatch } = useContext(AppContext);
   const { location, onChange } = useForms({
     location: "",
   });
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    dispatch(addLocation(location));
+    onClose();
   };
 
   return (
-    <div>
+    <div className="py-5 px-5 rounded bg-[#2e2c57]">
       <form onSubmit={onSubmit}>
         <div className="flex border-[3px] border-oxfordBlue-300 rounded">
           <input
-            className="bg-oxfordBlue-600 w-full py-3 pl-5 focus:outline-0"
+            className="bg-oxfordBlue-300 w-full py-3 pl-5 focus:outline-0"
             type="text"
             placeholder="Ingrese una ubicación"
             value={location}
@@ -25,9 +33,9 @@ const SearchInput = () => {
               onChange(target.value, "location");
             }}
           />
-          <div className="p-1 flex">
+          <div className="p-1 flex bg-oxfordBlue-300">
             <button
-              className="p-2 focus:outline-0 focus:bg-[#3c3b56] hover:bg-[#3c3b56] rounded-full"
+              className="p-2 focus:outline-0 focus:bg-[#2e2c57] hover:bg-[#2e2c57] rounded-full"
               type="submit"
             >
               <SendIcon color="transparent" />
